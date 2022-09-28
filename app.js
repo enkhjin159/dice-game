463a891815535b50c4cd29fc5df483a30265a690
enjii <h.enkhjin24@yahoo.com>
2022-09-28 15:01:29 +0800

    shoog shiddeg bolgoloo

--- app.js
+++ app.js
@@ -13,20 +13,22 @@ var scores = [0 , 0];
 var roundScore = 0;
 
 // shoni nudiig hadgalah magadlalaar 1-6
-var dice =Math.floor(Math.random() * 6) + 1;
+var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-// document.querySelector('#score-0').textContent = dice;
-
-// document.querySelector('#score-1').innerHTML = "<em>Yes!</em>";
 
 // program ehlehed
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function (){
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    
+    diceDom.style.display = "block";
+    diceDom.src ='dice-' + diceNumber + '.png';
 
-document.querySelector(".dice").style.display = "none";
+});
 
-
-console.log("shoo :" + dice);
